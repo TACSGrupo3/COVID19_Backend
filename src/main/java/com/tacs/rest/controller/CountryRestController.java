dc6b0eea7d383842bf5305085d6c428e6dd3be5c
francoalopez <fran.lopez44@hotmail.com>
2020-04-15 20:31:15 -0300

    Se modifica el servicio de paises cercanos

--- src/main/java/com/tacs/rest/controller/CountryRestController.java
+++ src/main/java/com/tacs/rest/controller/CountryRestController.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tacs.rest.entity.CountriesList;
@@ -17,14 +17,46 @@ import com.tacs.rest.entity.Region;
 import com.tacs.rest.entity.User;
 
 @RestController
-@RequestMapping("/countries") 
 public class CountryRestController {
 
 //	@Autowired
 //	private CountriesService countriesService;
-	
+		
 	@GetMapping("/countries")
-	public List<Country> findAll(){
+	public List<Country> listaDePaisesCercanos(@RequestParam(required = false) String near){
+		
+		if(near != null) {
+			//retornará la lista de paises cercanos por region
+//			return paisesService.findNearCountrys();
+			
+			//MOCK
+			List<Country> countries = new ArrayList<Country>();
+			Region regionAmSur = new Region();
+			regionAmSur.setId(1);
+			regionAmSur.setNameRegion("America del Sur");
+			regionAmSur.setSubRegion("Sur");
+
+			Country brasil = new Country();
+			brasil.setId(1);
+			brasil.setName("Brasil");
+			brasil.setRegion(regionAmSur);
+		
+			Country uruguay = new Country();
+			uruguay.setId(2);
+			uruguay.setName("Uruguay");
+			uruguay.setRegion(regionAmSur);
+
+			Country chile = new Country();
+			chile.setId(3);
+			chile.setName("Chile");
+			chile.setRegion(regionAmSur);
+			
+			countries.add(uruguay);
+			countries.add(chile);
+			countries.add(brasil);
+			
+			return countries;
+		}else {
 		//retornará todos los paises
 //		return countriesService.findAll();
 		
@@ -48,41 +80,10 @@ public class CountryRestController {
 		countries.add(chile);
 		
 		return countries;
-	}
-	
-	
-	@GetMapping("/region/{idRegion}")
-	public List<Country> listaDePaisesCercanos(@PathVariable int idRegion){
-		//retornará la lista de paises cercanos por region
-//		return paisesService.findNearCountrys();
+		}
 		
-		//MOCK
-		List<Country> countries = new ArrayList<Country>();
-		Region regionAmSur = new Region();
-		regionAmSur.setId(1);
-		regionAmSur.setNameRegion("America del Sur");
-		regionAmSur.setSubRegion("Sur");
-
-		Country brasil = new Country();
-		brasil.setId(1);
-		brasil.setName("Brasil");
-		brasil.setRegion(regionAmSur);
-	
-		Country uruguay = new Country();
-		uruguay.setId(2);
-		uruguay.setName("Uruguay");
-		uruguay.setRegion(regionAmSur);
-
-		Country chile = new Country();
-		chile.setId(3);
-		chile.setName("Chile");
-		chile.setRegion(regionAmSur);
 		
-		countries.add(uruguay);
-		countries.add(chile);
-		countries.add(brasil);
 		
-		return countries;
 	}
 	
 	@PostMapping("/listCountries")
