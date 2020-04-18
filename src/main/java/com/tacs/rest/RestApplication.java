677ef55419d5e87744262fecb7e5dcfb59e5a0da
francoalopez <fran.lopez44@hotmail.com>
2020-04-18 14:30:05 -0300

    Se agrega lógica de token y una propiedad que habilita/deshabilita dicha lógica

--- src/main/java/com/tacs/rest/RestApplication.java
+++ src/main/java/com/tacs/rest/RestApplication.java
@@ -1,14 +1,28 @@
 package com.tacs.rest;
 
+import javax.ws.rs.HttpMethod;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.tacs.rest.security.JWTAuthorizationFilter;
 
 @SpringBootApplication
 public class RestApplication {
 
+	@Autowired
+	 private Environment env;
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
@@ -22,4 +36,21 @@ public class RestApplication {
 			}
 		};
 	}
+	
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			if(Boolean.valueOf(env.getProperty("token.enable"))) {
+				http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/session").permitAll()
+				.anyRequest().authenticated();
+			}
+		}
+	}
+	
 }
