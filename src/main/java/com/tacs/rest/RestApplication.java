b9354d7a00c30473688e8b5786a99e4ad27dda01
janice <janicefabulus@live.com.ar>
2020-05-17 21:40:19 -0300

    COMMIT 100!!!!!!!!!!!!! Perdon borre un Override je

--- src/main/java/com/tacs/rest/RestApplication.java
+++ src/main/java/com/tacs/rest/RestApplication.java
@@ -73,6 +73,7 @@ public class RestApplication {
 		
 		@Autowired
 		AuthenticationEntryPoint jwtAuthenticationEntryPoint;
+		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			
 			http.csrf().disable()
