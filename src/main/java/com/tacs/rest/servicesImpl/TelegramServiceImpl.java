cdcab28e6a47fca892eeb54f5c2f051803635322
Maximiliano Ricagno <ricagnom@gmail.com>
2020-05-19 10:49:37 -0300

    Avance funcionalidad Telegram

--- src/main/java/com/tacs/rest/servicesImpl/TelegramServiceImpl.java
+++ src/main/java/com/tacs/rest/servicesImpl/TelegramServiceImpl.java
@@ -1,19 +1,18 @@
 package com.tacs.rest.servicesImpl;
 
-import com.tacs.rest.controller.CountryRestController;
 import com.tacs.rest.entity.CountriesList;
 import com.tacs.rest.services.CountriesListService;
-import com.tacs.rest.services.CountryService;
+import com.tacs.rest.services.TelegramService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TelegramServiceImpl {
+public class TelegramServiceImpl implements TelegramService {
 
     @Autowired
     public CountriesListService countriesListService;
 
-
+    @Override
     public CountriesList countries_list(int user_id, int list_id) {
 
         return countriesListService.findByUserId(user_id).stream().filter(list_countries -> list_countries.getId() == list_id).findAny().orElse(null);
