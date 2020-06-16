42d0e68d21f02ea6936d2262bce5efe4fa528066
francoalopez <fran.lopez44@hotmail.com>
2020-06-16 20:34:05 -0300

    Se sacan los cambios de WebHook

--- src/main/java/com/tacs/rest/services/TelegramService.java
+++ src/main/java/com/tacs/rest/services/TelegramService.java
@@ -3,23 +3,12 @@ package com.tacs.rest.services;
 import com.tacs.rest.entity.CountriesList;
 import com.tacs.rest.entity.Country;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
 
 @Service
 public interface TelegramService {
 
-    void messageHandling(Update update) throws IOException;
-
     CountriesList countries_list(int user_id, int list_id);
-
     Country get_country_information(String name);
 
-    void set_message_listCountries(Update update, String action, String messageString, SendMessage message);
-
-    void send_to_telegram(Update update, String action_text, boolean alert, int cacheTime, SendMessage message) throws IOException;
 
 }
