c9fb5325ad9f7d9d9574dd5cc16e0df2a961609b
janice <janicefabulus@live.com.ar>
2020-06-29 01:25:25 -0300

    Puse el zip en data report

--- src/main/java/com/tacs/rest/servicesImpl/ReportServiceImpl.java
+++ src/main/java/com/tacs/rest/servicesImpl/ReportServiceImpl.java
@@ -14,9 +14,12 @@ import org.springframework.web.server.ResponseStatusException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class ReportServiceImpl implements ReportService {
@@ -27,44 +30,31 @@ public class ReportServiceImpl implements ReportService {
     CountryService countryServ;
 
     @Override
-    public List<Country> reportData(List<Integer> countries, List<String> offset) {
+    public List<Country> reportData(List<Integer> countries, List<String> offsets) {
 
         List<Country> countriesPedidos = countryServ.findCountriesByIds(countries);
         if (countriesPedidos.stream().anyMatch(c -> c == null)) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ingresó un país Inválido");
         }
-
-        List<Country> countriesToShow = new ArrayList<Country>();
-
-        for (int i = 0; i < countriesPedidos.size(); i++) {
-        	List<DataReport> dataToadd = new ArrayList<DataReport>();
-
-            Country country = countriesPedidos.get(i);
-            List<DataReport> dataReprt = drDAO.findByCountry_idCountryOrderByDateAsc(country.getIdCountry());
-
-            country.setDataReport(dataReprt);
+        
+        return Streams.zip(countriesPedidos.stream(), offsets.stream(), (country, offset) -> {
+        	       	
             DateFormat formatter = new SimpleDateFormat("dd/MM/yy");
             Date date;
             try {
-                date = formatter.parse(offset.get(i));
+                date = formatter.parse(offset);
             } catch (Exception e) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El formato de las fechas debe ser dd/mm/aaaa.");
             }
-
-            dataToadd = country.getDataReport().stream().filter(item -> {
+       	
+        	country.setDataReport(country.getDataReport().stream().filter(item -> {
                 return item.getDate().after(date) || item.getDate().getTime() == date.getTime();
-            }).collect(Collectors.toList());
-
-            country.setDataReport(dataToadd);
-            countriesToShow.add(country);
-        }
-
-
-       // countriesToShow = Streams.zip(countriesPedidos, (country2) ->
-        //).collect(Collectors.toList());
-
-
-        return countriesToShow;
+            }).sorted(Comparator.comparing(DataReport::getDate)).collect(Collectors.toList()));
+        	
+        	return country; 
+        	
+        }).collect(Collectors.toList());
+        
     }
 
     @Override
